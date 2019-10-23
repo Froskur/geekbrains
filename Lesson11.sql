da601f3f020347840982bffcde416890c8f55076
Andrey S <semashenkov@gmail.com>
2019-10-24 02:41:33 +0300

    Add files via upload

---
+++ Lesson11.sql
@@ -0,0 +1,80 @@
+# =======================================================
+# 1.
+# —оздайте таблицу logs типа Archive. 
+# ѕусть при каждом создании записи в таблицах users, catalogs и products в таблицу logs помещаетс€ 
+# врем€ и дата создани€ записи, название таблицы, идентификатор первичного ключа и содержимое пол€ name.
+
+DROP TABLE IF EXISTS logs;
+
+CREATE TABLE logs (
+	created_at DATETIME NOT NULL,
+	target_id BIGINT NOT NULL,
+	table_name ENUM('users', 'catalogs' ,'products') NOT NULL,
+	name VARCHAR(255)
+) ENGINE=Archive;
+
+# создаем тригер на каждую таблицу 
+DROP TRIGGER IF EXISTS trg_users_logs_create;
+DROP TRIGGER IF EXISTS trg_catalogs_logs_create;
+DROP TRIGGER IF EXISTS trg_products_logs_create;
+
+DELIMITER //
+
+CREATE TRIGGER trg_users_logs_create AFTER INSERT ON users
+FOR EACH ROW BEGIN
+   INSERT INTO logs SET created_at = NOW(), 
+   						target_id = NEW.id,
+   						table_name = 'users',
+   						name = NEW.name
+  	;
+END//
+
+CREATE TRIGGER trg_catalogs_logs_create AFTER INSERT ON catalogs
+FOR EACH ROW BEGIN
+   INSERT INTO logs SET created_at = NOW(), 
+   						target_id = NEW.id,
+   						table_name = 'catalogs',
+   						name = NEW.name
+  	;
+END//
+
+CREATE TRIGGER trg_products_logs_create AFTER INSERT ON products
+FOR EACH ROW BEGIN
+   INSERT INTO logs SET created_at = NOW(), 
+   						target_id = NEW.id,
+   						table_name = 'products',
+   						name = NEW.name
+  	;
+END//
+
+DELIMITER ;
+
+# =====================================================================================
+# 2.
+# (по желанию) —оздайте SQL-запрос, который помещает в таблицу users миллион записей.
+
+# —оздаем процедуру...
+
+DROP PROCEDURE IF EXISTS users_create_big_data;
+
+DELIMITER //
+
+CREATE PROCEDURE users_create_big_data (IN count_rows INT)
+  BEGIN 
+	DECLARE new_birthday VARCHAR(10);  
+	WHILE count_rows > 0 DO
+		# ƒобавил чтоб пользователи хоть немного различались	
+		SET new_birthday = CONCAT(FLOOR(RAND()*(2002-1960+1)+1960),'-',FLOOR(RAND()*(12-1+1)+1),'-',FLOOR(RAND()*(28-1+1)+1));	
+		
+		INSERT DELAYED INTO users SET name = 'Auto create', birthday_at = new_birthday;    	
+    	SET count_rows = count_rows - 1;
+  	END WHILE;	     
+	  
+END//
+  
+DELIMITER ;
+
+# ¬ызываем процедуру дл€ создани€ миллиона записей
+CALL users_create_big_data(10);
+
+INSERT DELAYED INTO users SET name = 'Auto create', birthday_at = '1979-10-11';
